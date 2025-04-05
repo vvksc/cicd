540048a4f4b2760382d6070f18900ffd93349066
Vivek <vc91789@gmail.com>
2025-04-06 00:27:20 +0530

    commit 2

--- src/main/java/com/cicd/CicdApplication.java
+++ src/main/java/com/cicd/CicdApplication.java
@@ -2,13 +2,22 @@ package com.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class CicdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdApplication.class, args);
 		System.out.println("Hello World");
+	}
+	@GetMapping
+	public String hello() {
+		return "Hello World";
 	}
 
 }
